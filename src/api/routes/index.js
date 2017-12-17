b6de7776c5e896ef1dd4dc6bd1b1fbc455531fa1
alexbo93 <alexbo93@bitbucket.git>
2017-12-17 18:56:17 +0100

    Controllers (in progress), models and db configuration

--- src/api/routes/index.js
+++
@@ -1,8 +0,0 @@
-import userRoutes from './user_routes';
-import policyRoutes from './policy_routes';
-
-// Let's export routes for each object
-export default (app, db) => {
-  userRoutes(app, db);
-  policyRoutes(app, db);
-}
